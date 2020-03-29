e58491b1cf996ae30bfab0adc423836409ee8c02
Jana E. Beck <jana.eliz.beck@gmail.com>
2020-03-29 01:51:04 -0700

    tweak page transition animations on mobile

--- src/components/link.js
+++ src/components/link.js
@@ -1,16 +1,25 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { MOBILE_WIDTH } from "typography-breakpoint-constants"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 
-import { bg } from "../utils/colors"
+import { bg, text } from "../utils/colors"
 
 export default function Link({ children, path }) {
+  const onMobile = window.matchMedia(`(max-width: ${MOBILE_WIDTH})`).matches
   if (!path.startsWith("/")) {
     throw new Error("Path should start with `/`!")
   }
 
   return (
-    <AniLink bg={bg} cover direction="up" to={path}>
+    <AniLink
+      bg={bg}
+      hex={text}
+      cover={!onMobile}
+      direction="up"
+      paintDrip={onMobile}
+      to={path}
+    >
       {children}
     </AniLink>
   )
