f612fed5d84231c3976252fb8fe9708827e9b81b
Jana E. Beck <jana.eliz.beck@gmail.com>
2020-04-02 00:50:10 -0700

    fix height of background on mobile (Safari)

--- src/components/layout.js
+++ src/components/layout.js
@@ -72,6 +72,8 @@ const Global = createGlobalStyle`
     ${analogous[4]} 86.67%
   )`
         : bg};
+    background-size: ${({ isOnMobile }) => (isOnMobile ? "125%" : "100%")};
+    height: ${({ isOnMobile }) => (isOnMobile ? "125%" : "100%")};
   }
   :focus {
     outline-color: #F9ADA0;
