cd492cd459a20b82950cf2b870f443c6bd90c825
Jana E. Beck <jana.eliz.beck@gmail.com>
2020-04-05 14:37:26 -0700

    fix window access for builds

--- src/utils/mobileMediaQuery.js
+++ src/utils/mobileMediaQuery.js
@@ -2,12 +2,17 @@ const minWidth = "320px"
 const maxWidth = "812px"
 
 const isMobileDevice =
+  typeof window != "undefined" &&
   window.matchMedia(`(min-device-width: ${minWidth})`).matches &&
   window.matchMedia(`(max-device-width: ${maxWidth})`).matches
 
 const onMobileLandscape =
-  isMobileDevice && window.matchMedia("(orientation: landscape)").matches
+  typeof window != "undefined" &&
+  isMobileDevice &&
+  window.matchMedia("(orientation: landscape)").matches
 const onMobilePortrait =
-  isMobileDevice && window.matchMedia("(orientation: portrait)").matches
+  typeof window != "undefined" &&
+  isMobileDevice &&
+  window.matchMedia("(orientation: portrait)").matches
 
 export const isOnMobile = onMobileLandscape || onMobilePortrait
