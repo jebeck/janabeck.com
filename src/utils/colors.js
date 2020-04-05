57b17e660e53ccb36e377a3a38670462bc169563
Jana E. Beck <jana.eliz.beck@gmail.com>
2020-04-05 10:10:32 -0700

    tweak colors

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,9 +1,10 @@
 const colors = {
   accent: "#F9ADA0",
-  analogous: ["#FF7C4F", "#E85948", "#FF5C72", "#E848AD", "#F24FFF"],
+  analogous: ["#FF7C4F", "#E85948", "#FF5D73", "#E848AD", "#F24FFF"],
   bg: "#461A20",
   border: "#494949",
   text: "#FF5D73",
+  darkText: "#CC495B",
   textBg: "#232323",
 }
 
